d3adef61dbaaa0be98fa716906fe8775ee35918d
webdev-alexey <alexey.anasov@yandex.by>
2024-02-07 21:54:48 +0300

    Отправка вакансии на сервер

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://workspace-methed.vercel.app/";
+const API_URL = "https://pleasant-morning-plaster.glitch.me/";
 const LOCATION_URL = "api/locations";
 const VACANCY_URL = "api/vacancy";
 
@@ -359,6 +359,8 @@ const init = () => {
         .addRequiredGroup("#format", "Выберите формат")
         .addRequiredGroup("#experience", "Выберите опыт")
         .addRequiredGroup("#type", "Выберите занятость");
+
+      return validate;
     };
 
     const fileControler = () => {
@@ -382,11 +384,32 @@ const init = () => {
 
     const formControler = () => {
       const form = document.querySelector(".employer__form");
+      const employerError = document.querySelector(".employer__error");
+      const validate = validationForm(form);
 
-      validationForm(form);
-
-      form.addEventListener("submit", (event) => {
+      form.addEventListener("submit", async (event) => {
         event.preventDefault();
+
+        if (!validate.isValid) {
+          return;
+        }
+
+        try {
+          const formData = new FormData(form);
+          employerError.textContent = "Отправка, подождите...";
+          const response = await fetch(`${API_URL}${VACANCY_URL}`, {
+            method: "POST",
+            body: formData,
+          });
+
+          if (response.ok) {
+            employerError.textContent = "";
+            window.location.href = "index.html";
+          }
+        } catch (error) {
+          employerError.textContent = "Произошла ошибка";
+          console.error(error);
+        }
       });
     };
 
